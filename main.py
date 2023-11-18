afb881631d92f4db183dfdbe7abbcf7d875e4385
kevinloh <e0439232@u.nus.edu>
2023-11-18 19:22:06 +0800

    implemented an sqlite database to store item id

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ from bs4 import BeautifulSoup
 import time
 from selenium.webdriver.chrome.service import Service
 from telegram.error import RetryAfter
+import sqlite3
+
+conn = sqlite3.connect('items.db')
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS items (id TEXT PRIMARY KEY)''')
+conn.commit()
+
+def is_item_sent(item_id):
+    cursor.execute("SELECT id FROM items WHERE id = ?", (item_id,))
+    return cursor.fetchone() is not None
+
+def mark_item_as_sent(item_id):
+    cursor.execute("INSERT INTO items (id) VALUES (?)", (item_id,))
+    conn.commit()
 
 async def main():
     bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
@@ -17,19 +31,24 @@ async def main():
 
     url = 'https://www.passiton.org.sg/item-list'
     driver.get(url)
-
+    i = 0
     while True:
+        i = i + 1
         time.sleep(5)
         source = driver.page_source
         soup = BeautifulSoup(source, 'html.parser')
 
         rows = soup.find_all('tr', class_='lineEven') + soup.find_all('tr', class_='lineOdd')
-        i = 0
         for row in rows:
             i = i + 1
-            if(i == 5): break
+            if i == 5: break
             id_cell = row.find('td', style='width:20px;')
             id = id_cell.text.strip() if id_cell else 'NA'
+
+            if is_item_sent(id):
+                continue
+            else:
+                mark_item_as_sent(id)
 
             name_desc_tag = row.find('td').find_next_sibling('td')
             name_desc = name_desc_tag.text.strip() if name_desc_tag else 'NA'
@@ -41,8 +60,8 @@ async def main():
             location_tag = name_desc_tag.find_next_sibling('td')
             location = location_tag.text.strip() if location_tag else 'NA'
 
-            validity_td = row.find_all('td')[4]
-            validity_item_age_dimension_text = validity_td.get_text(separator="\n", strip=True)
+            validity_item_age_dimension_td = row.find_all('td')[4]
+            validity_item_age_dimension_text =  validity_item_age_dimension_td.get_text(separator="\n", strip=True)
             lines = validity_item_age_dimension_text.split('\n')
             validity_text = lines[0]
             age_text = lines[1] if len(lines) > 1 else "NA"
@@ -60,6 +79,7 @@ async def main():
             img_src = row.find('td', nowrap='nowrap').find('img')['src']
             img_base_url = 'https://www.passiton.org.sg'
             full_img_url = img_base_url + img_src
+            print(full_img_url)
             try:
                 await bot.send_photo(chat_id=channel_id, photo=full_img_url, caption=caption, parse_mode="Markdown")
             except RetryAfter as e:
